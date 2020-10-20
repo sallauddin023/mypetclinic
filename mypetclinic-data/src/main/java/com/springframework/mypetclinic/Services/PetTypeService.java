6eab763fda6391432184831c117d0c3fab6ccb0f
sallauddin023 <skallauddin023@gmail.com>
2020-10-20 12:26:04 +0530

    added petType service, closes #29

---
+++ mypetclinic-data/src/main/java/com/springframework/mypetclinic/Services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.springframework.mypetclinic.Services;
+
+import com.springframework.mypetclinic.Model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
