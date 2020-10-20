79e8a264d2747ad2edf23bee4c0198bec41f74c0
sallauddin023 <skallauddin023@gmail.com>
2020-10-20 12:27:36 +0530

    added petMap service, closes #29

---
+++ mypetclinic-data/src/main/java/com/springframework/mypetclinic/Services/Map/PetTypeMapService.java
@@ -0,0 +1,52 @@
+package com.springframework.mypetclinic.Services.Map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.springframework.mypetclinic.Model.PetType;
+import com.springframework.mypetclinic.Services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapServices<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findall() {
+		// TODO Auto-generated method stub
+		return super.findall();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+
+}
