3cf7187f13413dd699d486482c7427ffd99a7c2c
sallauddin023 <skallauddin023@gmail.com>
2020-10-20 12:10:04 +0530

    added speciality, added to vet, closes #27

--- mypetclinic-data/src/main/java/com/springframework/mypetclinic/Model/Vet.java
+++ mypetclinic-data/src/main/java/com/springframework/mypetclinic/Model/Vet.java
@@ -1,4 +1,18 @@
 package com.springframework.mypetclinic.Model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+	
+	private Set<Speciality> speialities;
+
+	public Set<Speciality> getSpeialities() {
+		return speialities;
+	}
+
+	public void setSpeialities(Set<Speciality> speialities) {
+		this.speialities = speialities;
+	}
+	
+	
 }
