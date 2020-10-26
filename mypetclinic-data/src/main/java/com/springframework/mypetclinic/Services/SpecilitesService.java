9be724e622bdc3ed0da54024394c882f75b8cc16
sallauddin023 <skallauddin023@gmail.com>
2020-10-26 15:05:18 +0530

    Added JPA

--- mypetclinic-data/src/main/java/com/springframework/mypetclinic/Services/SpecilitesService.java
+++
@@ -1,7 +0,0 @@
-package com.springframework.mypetclinic.Services;
-
-import com.springframework.mypetclinic.Model.Speciality;
-
-public interface SpecilitesService extends CrudService<Speciality, Long> {
-
-}
