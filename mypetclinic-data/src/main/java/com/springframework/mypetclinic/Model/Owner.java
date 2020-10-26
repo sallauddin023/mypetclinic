506b6265abcaa4d7b82a33bfe6be7b1411c28a27
sallauddin023 <skallauddin023@gmail.com>
2020-10-26 12:32:49 +0530

    pdated owner with JPA, closes #36

--- mypetclinic-data/src/main/java/com/springframework/mypetclinic/Model/Owner.java
+++ mypetclinic-data/src/main/java/com/springframework/mypetclinic/Model/Owner.java
@@ -3,11 +3,26 @@ package com.springframework.mypetclinic.Model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Owners")
 public class Owner extends Person {
+	
+	@Column(name = "address")
 	private String address;
+	
+	@Column(name = "city")
 	private String city;
+	
+	@Column(name = "telephone")
 	private String telephone;
 	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy ="owner")
 	private Set<Pet> pets = new HashSet<>();
 
 	public String getAddress() {
